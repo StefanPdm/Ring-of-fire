d23ea91838acba65a2947ef77c32fbfa5b19c766
Stefan Heinemann <115097982+StefanPdm@users.noreply.github.com>
2023-02-23 18:32:10 +0100

    start game logic

---
+++ src/app/game/game.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { Game } from 'src/models/game';
+
+@Component({
+  selector: 'app-game',
+  templateUrl: './game.component.html',
+  styleUrls: ['./game.component.scss'],
+})
+export class GameComponent {
+  pickCardAnimation = false;
+  game: Game | any;
+  currentCard: string = '';
+
+  ngOnInit(): void {
+    this.newGame();
+  }
+
+  newGame() {
+    this.game = new Game();
+    console.log(this.game);
+  }
+
+  takeCard() {
+    if (!this.pickCardAnimation) {
+      this.currentCard = this.game.stack.pop();
+      this.game.playedCards.push(this.currentCard);
+      this.pickCardAnimation = true;
+      console.log(this.currentCard);
+      setTimeout(() => {
+        this.pickCardAnimation = false;
+      }, 1500);
+    }
+  }
+}
